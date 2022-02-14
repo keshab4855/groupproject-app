bf67479c5a5dc4b8dbde39160302246defddb1a0
keshab4855 <91373317+keshab4855@users.noreply.github.com>
2022-02-15 04:52:52 +1100

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -31,16 +31,20 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is ready");
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Server is ready");
+//   });
+// }
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 // app.get("/", (req, res) => {
 //   res.send("Server is ready");
